de7bbad2191c7a9c8f89ec4da97d04245b16d68f
Jin Eguchi <wakiyamap@gmail.com>
2019-01-11 04:50:14 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -48,6 +48,10 @@ displayErr() {
     sudo apt-get -y upgrade
     sudo apt-get -y autoremove
     
+    # for mcrypt on php7.2
+    sudo apt-get -y install php7.2-dev libmcrypt-dev
+    sudo pecl install mcrypt-1.0.1
+    echo "extension=mcrypt.so" > sudo tee --append /etc/php/7.2/mods-available/mcrypt.ini
     
     # Switch Aptitude
     output " "
@@ -94,6 +98,7 @@ default         0;
     rootpasswd=$(openssl rand -base64 12)
     export DEBIAN_FRONTEND="noninteractive"
     sudo aptitude -y install mariadb-server
+    sudo aa-remove-unknown
     sudo systemctl start mysql
     sudo systemctl enable mysql
     
@@ -105,8 +110,8 @@ default         0;
     sleep 3
     
     sudo aptitude -y install php7.2-fpm
-    sudo aptitude -y install php7.2-opcache php7.2-fpm php7.2 php7.2-common php7.2-gd php7.2-mysql php7.2-imap php7.2-cli php7.2-cgi php-pear php-auth php7.2-mcrypt mcrypt imagemagick libruby php7.2-curl php7.2-intl php7.2-pspell php7.2-recode php7.2-sqlite3 php7.2-tidy php7.2-xmlrpc php7.2-xsl memcached php-memcache php-imagick php-gettext php7.2-zip php7.2-mbstring
-    sudo phpenmod mcrypt
+    sudo aptitude -y install php7.2-opcache php7.2-fpm php7.2 php7.2-common php7.2-gd php7.2-mysql php7.2-imap php7.2-cli php7.2-cgi php-pear php-auth-sasl imagemagick libruby php7.2-curl php7.2-intl php7.2-pspell php7.2-recode php7.2-sqlite3 php7.2-tidy php7.2-xmlrpc php7.2-xsl memcached php-memcache php-imagick php-gettext php7.2-zip php7.2-mbstring
+    sudo phpenmod mcrypto
     sudo phpenmod mbstring
     sudo systemctl start php7.2-fpm.service
     
